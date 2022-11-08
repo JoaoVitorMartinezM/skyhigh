9895c880f84282f9a1f620f761a5aa8f44c959a8
JoaoVitorMartinez <justdatascientist@gmail.com>
2022-11-08 09:47:53 -0300

    microsservicos funcionando

--- md-produto/src/main/java/com/skyhigh/produto/repositories/ProdutoRepo.java
+++
@@ -1,7 +0,0 @@
-package com.skyhigh.produto.repositories;
-
-import com.skyhigh.produto.models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepo extends JpaRepository<Produto, Long> {
-}
